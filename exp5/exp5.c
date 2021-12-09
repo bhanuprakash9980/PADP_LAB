27843bd73e2d91fee91acbda8fb9c3d759817ab5
Bhanu Prakash <53945083+bhanuprakash9980@users.noreply.github.com>
2021-12-10 05:13:25 +0530

    Update exp5.c

--- exp5/exp5.c
+++ exp5/exp5.c
@@ -13,18 +13,18 @@ int main(int argc,char **argv)
  MPI_Status status;
  MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
  MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
+ if(myRank==1||myRank==2||myRank==3)
+ {MPI_Recv(&mess,BUFSIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
+  printf("\n%s is message process of rank %d sent to process of rank %d\n",mess,myRank,root);
+ }
  if(myRank==0)
- {
-  for(temp=1;temp<numProcs;temp++)
+ {for(temp=1;temp<numProcs;temp++)
   {
    strcpy(mess,m[temp-1]);
    MPI_Send(&mess,BUFSIZE,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
   } 
- }
- else
- {
-  MPI_Recv(&mess,BUFSIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
-  printf("\n%s is message process of rank %d sent to process of rank %d\n",mess,myRank,root);
+  
+  
  }
  MPI_Finalize();
  return 0;
