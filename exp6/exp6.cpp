9e116b4f6183da829bfdaff56887ec827c1194b3
Bhanu Prakash <53945083+bhanuprakash9980@users.noreply.github.com>
2021-12-10 06:32:37 +0530

    Update exp6.cpp

--- exp6/exp6.cpp
+++ exp6/exp6.cpp
@@ -2,6 +2,50 @@
 #include<fstream>
 #include<cstring>
 #include<omp.h>
+#include<ctype.h>
+
+using namespace std;
+
+
+
+int strcmpi(string a,string b)
+{ int len_a=a.length(),len_b= b.length();
+	if(len_a!=len_b)
+		return -1;
+	for(int i=0;i<len_a;i++)
+	{
+		if(tolower(a[i])!=tolower(b[i]))
+			return -1;
+	}
+ return 0;
+}
+int main(){
+string filename = {"words.txt"};
+string word[7] = {"lorem", "ipsum", "dolor", "sit","amet","hi","good"};
+double st=omp_get_wtime();
+    
+        for(int j=1;j<=8;j*=2){
+            #pragma omp parallel for shared(word) num_threads(j) 
+            for(int k=0;k<7;k++){
+                int count = 0;
+                fstream file;
+                string word1;
+                file.open(filename.c_str());
+                while (file >> word1)
+                {
+                    if(strcmpi(word[k],word1)==0)
+                        count++;
+                }
+                cout<<count<<" "<<word[k]<<endl;
+            }
+		cout<<"Time "<<omp_get_wtime()-st<<endl;
+
+        }
+	
+    }#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<omp.h>
 
 using namespace std;
 
