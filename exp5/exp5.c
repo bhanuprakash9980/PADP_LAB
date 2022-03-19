087f44e822003f2008ee183680a680d387e6b2ff
Bhanu Prakash <53945083+bhanuprakash9980@users.noreply.github.com>
2022-03-19 19:31:49 +0530

    Update exp5.c
    This had a bug now sorted

--- exp5/exp5.c
+++ exp5/exp5.c
@@ -1,31 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-#include <mpi.h>
-#define BUFSIZE 32 
-char m[5][20]={"\nHello","\nRVCE","\nCSE","\n7th Sem","\nHPC"};
-int main(int argc,char **argv)
+#include<mpi.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+char mess[5][20]={"Hello","RVCE","CSE","NEW"};
+
+#define BUFSIZE 32
+
+int main(int argc,char** argv)
 {
- char mess[32];
- int root=0;
- int tag =0;
- int myRank,numProcs,temp=1;
- MPI_Init(&argc,&argv);
- MPI_Status status;
- MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
- MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
- if(myRank==1||myRank==2||myRank==3)
- {MPI_Recv(&mess,BUFSIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
-  printf("\n%s is message process of rank %d received from process of rank %d\n",mess,myRank,root);
- }
- if(myRank==0)
- {for(temp=1;temp<numProcs;temp++)
-  {
-   strcpy(mess,m[temp-1]);
-   MPI_Send(&mess,BUFSIZE,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
-  } 
-  
-  
- }
- MPI_Finalize();
- return 0;
+	int root=0,tag=0,rank,numProcs;
+	char mes[32];
+	MPI_Init(&argc,&argv);
+	MPI_Status status;
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
+	if(rank!=0)
+	{
+		strcpy(mes,mess[rank-1]);
+		MPI_Send(&mes,BUFSIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD);
+	}
+	else
+	{
+		for(int i=1;i<numProcs;i++)
+		{	MPI_Recv(&mes,BUFSIZE,MPI_CHAR,i,tag,MPI_COMM_WORLD,&status);
+			printf("\n%s is message from %d process: ",mes,i);
+		}
+	}
+	
+	MPI_Finalize();
+	return 0;
 }
